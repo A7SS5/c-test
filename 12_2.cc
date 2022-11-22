13a42e361dfece4f175158fbeadc3c86fa411f72
A7SS5 <dengpeng==123>
2022-11-22 22:31:23 +0800

    智能指针TEST

--- 12_2.cc
+++ 12_2.cc
@@ -13,11 +13,33 @@ class StrBlob
     size_type size() const {return data->size();};
     bool empty()const { return data->size()==0?true:false;};
     void push_back(const string &t){data->push_back(t);};
-    void pop_back(){if(empty()==true) return; data->pop_back();};
+    void check(size_type t,const string&msg)
+    {
+        if(t>=data->size())
+        {
+            throw out_of_range(msg);
+        }
+    }
+    void pop_back(){
+        check(0,"pop_back on empty");
+        data->pop_back();
+     };
+     string &front()
+     {
+        check(0,"front on empty");
+        return data->front();
+     }
+     string &back()
+     {
+         check(0,"back on empty");
+        return data->back();
+     }
     private:
     shared_ptr<vector<string>> data;
 };
 int main()
 {
-
+    StrBlob array1;
+    cout<<array1.front();
+    
 }
